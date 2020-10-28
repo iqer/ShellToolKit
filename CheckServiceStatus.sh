875dbf5717346e1f0d71cf20476d91aeee6f465b
ArthurChen <arthur.chen@ucloud.cn>
2020-10-28 10:30:42 +0800

    fix(shell): shell语法格式化调整

--- CheckServiceStatus.sh
+++ CheckServiceStatus.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 IPs=${1}
-  
+
 if [ -z ${IPs} ]; then
     echo 'Need IP list'
     exit 1
 fi
 
-iplist=(`echo "${IPs}" | tr ',' ' '`)
+iplist=($(echo "${IPs}" | tr ',' ' '))
 
 for ip in ${iplist[@]}; do
-    ssh root@${ip} "ps -ef |grep -v grep| grep udatabase && ifconfig eth0 | grep inet | head -1 | awk '{print$2}'" >> 1.demo
+    ssh root@${ip} "ps -ef |grep -v grep| grep udatabase && ifconfig eth0 | grep inet | head -1 | awk '{print$2}'" >>1.demo
 done
